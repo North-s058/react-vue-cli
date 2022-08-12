8db09f3b8527fc9c35260f373d285f1281603655
North <839767891@qq.com>
2022-08-13 00:25:10 +0800

    vue_cli 项目搭建结束

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,16 +1,18 @@
-//各种引入
+//生产版
 const path = require("path"); //路径模块
 const ESLintWebpackPlugin = require("eslint-webpack-plugin"); //代码规范插件
+const MinimizerWebpackPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin"); //html兼容性插件
 const { VueLoaderPlugin } = require("vue-loader");
 const { DefinePlugin } = require("webpack");
 const CopyPlugin = require("copy-webpack-plugin");
 const { rules } = require("eslint-plugin-vue");
 //复制文件插件(不通过webpack打包)
+const isProduction = process.env.NODE_ENV === "production";
 
 const getStyleLoaders = (preProcessor) => {
   return [
-    "vue-style-loader",
+    isProduction ? MinimizerWebpackPlugin.loader : "vue-style-loader",
     "css-loader",
     {
       loader: "postcss-loader",
@@ -29,9 +31,13 @@ module.exports = {
   //出入口配置
   entry: "./src/main.js",
   output: {
-    path: undefined, //开发模式为虚拟磁盘,不用指定
-    filename: "static/js/[name].js", //输出文件名
-    chunkFilename: "static/js/[name].chunk.js", //切割文件名
+    path: isProduction ? path.resolve(__dirname, "../dist") : undefined, //开发模式为虚拟磁盘,不用指定
+    filename: isProduction
+      ? "static/js/[name].[contenthash:10].js"
+      : "static/js/[name].js", //输出文件名
+    chunkFilename: isProduction
+      ? "static/js/[name].[contenthash:10].chunk.js"
+      : "static/js/[name].chunk.js", //切割文件名
     assetModuleFilename: "static/js/[hash:10][ext][query]", //静态资源文件名
   },
   //模块配置
@@ -54,15 +60,7 @@ module.exports = {
         test: /\.styl$/,
         use: getStyleLoaders("stylus-loader"),
       },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024, //小的图片会被转成base64格式提升加载速度
-          },
-        },
-      },
+
       {
         test: /\.js$/,
         include: path.resolve(__dirname, "../src"),
@@ -101,29 +99,35 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "../public/index.html"),
     }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "../public"),
-          to: path.resolve(__dirname, "../dist"),
-          toType: "dir",
-          noErrorOnMissing: true,
-          globOptions: {
-            ignore: ["**/index.html"],
+    isProduction &&
+      new MinimizerWebpackPlugin({
+        filename: "static/css/[name].[contenthash:10].css",
+        chunkFilename: "static/css/[name].[contenthash:10].chunk.css",
+      }),
+    isProduction &&
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "../public"),
+            to: path.resolve(__dirname, "../dist"),
+            toType: "dir",
+            noErrorOnMissing: true,
+            globOptions: {
+              ignore: ["**/index.html"],
+            },
+            info: {
+              minimized: true,
+            },
           },
-          info: {
-            minimized: true,
-          },
-        },
-      ],
-    }),
+        ],
+      }),
     new VueLoaderPlugin(),
     new DefinePlugin({
       //定义环境变量,给源代码使用,. cross-env给打包工具使用的
       __VUE_OPTIONS_API__: "true",
-      __VUE_PROD_DEVTOOLS: "true",
+      __VUE_PROD_DEVTOOLS: "false",
     }),
-  ],
+  ].filter(Boolean),
   //优化
   optimization: {
     //切割
@@ -134,6 +138,8 @@ module.exports = {
     runtimeChunk: {
       name: (entrypoint) => `runtime~${entrypoint.name}`,
     },
+    minimize: isProduction,
+    minimizer: [],
   },
   //解析
   resolve: {
@@ -149,6 +155,6 @@ module.exports = {
     compress: true,
     historyApiFallback: true, // 解决vue-router刷新404问题
   },
-  mode: "development", //开发模式
-  devtool: "cheap-module-source-map", //生成map文件,体现打包前后的文件的对应关系
+  mode: isProduction ? "production" : "development", //开发模式
+  devtool: isProduction ? "source-map" : "cheap-module-source-map", //生成map文件,体现打包前后的文件的对应关系
 };
